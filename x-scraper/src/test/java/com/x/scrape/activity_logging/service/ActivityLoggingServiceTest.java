47d52dfaf0911c4b52767542b0dfcee91d1d860a
SpookyDooky <SpookDooky@gmail.com>
2025-10-13 21:59:27 +0200

    Finished final test for activity logging system

--- x-scraper/src/test/java/com/x/scrape/activity_logging/service/ActivityLoggingServiceTest.java
+++ x-scraper/src/test/java/com/x/scrape/activity_logging/service/ActivityLoggingServiceTest.java
@@ -1,29 +1,39 @@
 package com.x.scrape.activity_logging.service;
 
 import com.x.scrape.activity_logging.event.ActivityEvent;
+import com.x.scrape.activity_logging.event.ActivityFlushEvent;
 import com.x.scrape.activity_logging.model.Activity;
 import com.x.scrape.activity_logging.model.RequestActivity;
 import com.x.scrape.logging.ContextLogger;
 import com.x.scrape.model.job.Job;
 import com.x.scrape.scraping.job.JobRegistry;
 import org.instancio.Instancio;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.context.ApplicationEventPublisher;
 
+import java.net.URL;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 import static com.x.scrape.logging.ContextKeys.JOB_ID;
+import static com.x.scrape.logging.ContextKeys.URL;
 import static org.instancio.Select.field;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ActivityLoggingServiceTest {
 
-	@Mock
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd_hh-mm-ss");
+
+    @Mock
 	private ContextLogger logger;
 	@Mock
 	private ApplicationEventPublisher applicationEventPublisher;
@@ -32,7 +42,10 @@ class ActivityLoggingServiceTest {
 	
 	@InjectMocks
 	private ActivityLoggingService activityLoggingService;
-	
+
+    @Captor
+    private ArgumentCaptor<ActivityFlushEvent> eventArgumentCaptor;
+
 	@Test
 	void shouldHandleActivityEvent() {
 		final UUID jobId = UUID.randomUUID();
@@ -47,16 +60,28 @@ class ActivityLoggingServiceTest {
 		
 		final Job job = Instancio.create(Job.class);
 		when(jobRegistry.get(jobId)).thenReturn(job);
-		
-		// Flush all activities
-		activityLoggingService.flushActivityLogs();
+
+        final Instant currentTime = Instant.now();
+        try (final MockedStatic<Instant> mockedInstant = mockStatic(Instant.class)) {
+            mockedInstant.when(Instant::now).thenReturn(currentTime);
+
+            // Flush all activities
+            activityLoggingService.flushActivityLogs();
+
+            verify(applicationEventPublisher).publishEvent(eventArgumentCaptor.capture());
+            final ActivityFlushEvent actualEvent = eventArgumentCaptor.getValue();
+
+            assertEquals(job.getJobFolder() + "/logs", actualEvent.getStorageHint().getFolder());
+            assertEquals(
+                    DATE_TIME_FORMATTER.format(LocalDateTime.ofInstant(currentTime, ZoneId.systemDefault())) + ".json",
+                    actualEvent.getStorageHint().getFileName()
+            );
+        }
 		
 	}
 	
 	RequestActivity createActivity(final UUID jobId) {
-		final RequestActivity activity =  Instancio.of(RequestActivity.class)
-				.set(field(RequestActivity::getUrl), mock())
-				.create();
+		final RequestActivity activity =  new RequestActivity(mock());
 		
 		activity.getContext()
 				.put(JOB_ID, jobId.toString());
