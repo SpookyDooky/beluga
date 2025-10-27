48ba9f43b3682f5f1d675e015ee0e45728e3ca7f
SpookyDooky <email@example.com>
2025-10-27 16:19:59 +0100

    Added configuration for datasource and entity manager factory

--- x-scraper/src/main/java/com/x/scrape/persistence/postgresql/config/PostgreSqlPersistenceConfig.java
+++ x-scraper/src/main/java/com/x/scrape/persistence/postgresql/config/PostgreSqlPersistenceConfig.java
@@ -1,11 +1,18 @@
 package com.x.scrape.persistence.postgresql.config;
 
 import com.x.scrape.persistence.config.conditionals.annotation.IsPostgreSql;
+import com.x.scrape.properties.persistence.PostgreSqlPersistenceProperties;
+import com.zaxxer.hikari.HikariDataSource;
 import jakarta.persistence.EntityManagerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @IsPostgreSql
 @Configuration
@@ -14,5 +21,31 @@ public class PostgreSqlPersistenceConfig {
 	@Bean
 	public PlatformTransactionManager transactionManager(final EntityManagerFactory emf) {
 		return new JpaTransactionManager(emf);
+	}
+	
+	@Bean
+	public DataSource dataSource(final PostgreSqlPersistenceProperties properties) {
+		final HikariDataSource dataSource = new HikariDataSource();
+		
+		dataSource.setJdbcUrl(properties.getUrl());
+		dataSource.setUsername(properties.getUsername());
+		dataSource.setPassword(properties.getPassword());
+		dataSource.setDriverClassName(properties.getDriverClassName());
+		
+		return dataSource;
+	}
+	
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(final DataSource dataSource,
+	                                                                   final JpaVendorAdapter jpaVendorAdapter) {
+		final LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+		entityManagerFactory.setDataSource(dataSource);
+		entityManagerFactory.setJpaVendorAdapter(jpaVendorAdapter);
+		
+		final Properties jpaProperties = new Properties();
+		jpaProperties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+		entityManagerFactory.setJpaProperties(jpaProperties);
+		
+		return entityManagerFactory;
 	}
 }
