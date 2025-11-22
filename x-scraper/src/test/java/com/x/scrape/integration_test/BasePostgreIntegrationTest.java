a73028627652abbe83e257058c34cc2b0fb43bfe
SpookyDooky <email@example.com>
2025-11-22 17:58:25 +0100

    Add base integration test for PostgreSQL

---
+++ x-scraper/src/test/java/com/x/scrape/integration_test/BasePostgreIntegrationTest.java
@@ -0,0 +1,25 @@
+package com.x.scrape.integration_test;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+
+@SpringBootTest
+@ActiveProfiles("postgresql")
+public class BasePostgreIntegrationTest {
+	
+	@Container
+	static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:17.0")
+			.withDatabaseName("test")
+			.withUsername("username")
+			.withPassword("password");
+	
+	@BeforeAll
+	static void setup() throws Exception {
+		postgreSQLContainer.start();
+		
+		System.setProperty("POSTGRE_HOST", postgreSQLContainer.getJdbcUrl());
+	}
+}
