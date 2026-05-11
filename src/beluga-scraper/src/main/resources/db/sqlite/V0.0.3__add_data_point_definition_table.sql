06c7b69fd0a814bfcc6bc85f67f5305d59222db8
SpookyDooky <SpookyDooky@mail.com>
2026-05-11 23:19:08 +0200

    fixed sqlite script by renaming property_name to field

--- src/beluga-scraper/src/main/resources/db/sqlite/V0.0.3__add_data_point_definition_table.sql
+++ src/beluga-scraper/src/main/resources/db/sqlite/V0.0.3__add_data_point_definition_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE data_point_definition (
 	id INTEGER PRIMARY KEY,
 	scraping_definition_id integer NOT NULL,
 	selector varchar NOT NULL,
-	property_name varchar NOT NULL,
+	field varchar NOT NULL,
 	"attribute" varchar NULL,
 	"type" varchar NOT NULL,
 	FOREIGN KEY (scraping_definition_id) REFERENCES scraping_definition(id)
