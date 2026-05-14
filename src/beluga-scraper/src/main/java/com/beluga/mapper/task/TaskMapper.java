2b1fba6a5b2b1c8396bc95106a53b8db88e16e26
SpookyDooky <SpookyDooky@mail.com>
2026-05-14 09:36:24 +0200

    Fix bug in task mapper

--- src/beluga-scraper/src/main/java/com/beluga/mapper/task/TaskMapper.java
+++ src/beluga-scraper/src/main/java/com/beluga/mapper/task/TaskMapper.java
@@ -1,19 +1,25 @@
 package com.beluga.mapper.task;
 
+import com.beluga.mapper.task.configuration.ScrapingConfigurationMapper;
 import com.beluga.model.job_definition.JobDefinition;
 import com.beluga.execution.model.task.Task;
 import com.beluga.execution.model.task.TaskDefinition;
+import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import static org.mapstruct.InjectionStrategy.CONSTRUCTOR;
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
 @Mapper(
-		componentModel = SPRING
+		componentModel = SPRING,
+		injectionStrategy = CONSTRUCTOR,
+		uses = {
+				ScrapingConfigurationMapper.class
+		}
 )
 public interface TaskMapper {
 
-	// Create separate mappers for this for better testability
 	@Mapping(target = "url", source = "taskDefinition.url")
 	@Mapping(target = "scrapingConfiguration", source = "jobDefinition.scrapingDefinition")
 	@Mapping(target = "storageConfiguration", source = "jobDefinition.storageDefinition")
--- src/beluga-scraper/src/test/java/com/beluga/mapper/task/TaskMapperTest.java
+++ src/beluga-scraper/src/test/java/com/beluga/mapper/task/TaskMapperTest.java
@@ -1,33 +1,50 @@
 package com.beluga.mapper.task;
 
+import com.beluga.execution.model.task.ScrapingConfiguration;
+import com.beluga.mapper.task.configuration.ScrapingConfigurationMapper;
 import com.beluga.model.job_definition.JobDefinition;
 import com.beluga.execution.model.task.Task;
 import com.beluga.execution.model.task.TaskDefinition;
 import org.instancio.Instancio;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.instancio.settings.Keys.COLLECTION_MAX_SIZE;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class TaskMapperTest {
-	
-	private final TaskMapper mapper = new TaskMapperImpl();
+
+	@Mock
+	private ScrapingConfigurationMapper scrapingConfigurationMapper;
+
+	@InjectMocks
+	private TaskMapperImpl mapper;
 	
 	@Test
 	void shouldMap() {
 		final TaskDefinition taskDefinition = Instancio.create(TaskDefinition.class);
-		final JobDefinition jobDefinition = Instancio.create(JobDefinition.class);
+		final JobDefinition jobDefinition = Instancio.of(JobDefinition.class)
+				.withSetting(COLLECTION_MAX_SIZE, 1)
+				.create();
+
+		final ScrapingConfiguration scrapingConfiguration = mock();
+		when(scrapingConfigurationMapper.map(jobDefinition.getScrapingDefinition())).thenReturn(scrapingConfiguration);
 		
 		final Task task = mapper.map(jobDefinition, taskDefinition);
 		
 		assertEquals(taskDefinition.getUrl(), task.getUrl());
 		assertEquals(jobDefinition.getStorageDefinition().getFolder(), task.getStorageConfiguration().getFolder());
 		assertEquals(jobDefinition.getStorageDefinition().getFormat(), task.getStorageConfiguration().getFormat());
-		assertEquals(jobDefinition.getScrapingDefinition().getItemSelector(), task.getScrapingConfiguration().getItemSelector());
 
-		assertEquals(
-				jobDefinition.getScrapingDefinition().getDataPointDefinitions().getFirst().getField(),
-				task.getScrapingConfiguration().getDataPointConfigurations().getFirst().getField()
-		);
+		assertSame(scrapingConfiguration, task.getScrapingConfiguration());
 	}
 	
 }
