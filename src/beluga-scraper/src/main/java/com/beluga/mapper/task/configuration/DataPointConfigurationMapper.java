370b337c6c515a7a0b73a705338bacb518f93e2c
SpookyDooky <email@example.com>
2026-08-09 10:03:09 +0200

    Modify data point mapper to include extraction configurations

--- src/beluga-scraper/src/main/java/com/beluga/mapper/task/configuration/DataPointConfigurationMapper.java
+++ src/beluga-scraper/src/main/java/com/beluga/mapper/task/configuration/DataPointConfigurationMapper.java
@@ -1,15 +1,23 @@
 package com.beluga.mapper.task.configuration;
 
 import com.beluga.execution.model.task.DataPointConfiguration;
+import com.beluga.mapper.task.configuration.extraction_configuration.ExtractionConfigurationMapper;
 import com.beluga.model.job_definition.configuration.scraping_configuration.DataPointDefinition;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+import static org.mapstruct.InjectionStrategy.CONSTRUCTOR;
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
 @Mapper(
-        componentModel = SPRING
+        componentModel = SPRING,
+        injectionStrategy = CONSTRUCTOR,
+        uses = {
+                ExtractionConfigurationMapper.class
+        }
 )
 public interface DataPointConfigurationMapper {
 
+    @Mapping(target = "extractionConfiguration", source = "extractionDefinition")
     DataPointConfiguration map(DataPointDefinition dataPointDefinition);
 }
---
+++ src/beluga-scraper/src/test/java/com/beluga/mapper/task/configuration/DataPointConfigurationMapperTest.java
@@ -0,0 +1,43 @@
+package com.beluga.mapper.task.configuration;
+
+import com.beluga.execution.model.task.DataPointConfiguration;
+import com.beluga.execution.model.task.extraction_configuration.ExtractionConfiguration;
+import com.beluga.mapper.task.configuration.extraction_configuration.ExtractionConfigurationMapper;
+import com.beluga.model.job_definition.configuration.scraping_configuration.DataPointDefinition;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class DataPointConfigurationMapperTest {
+
+    @Mock
+    private ExtractionConfigurationMapper extractionConfigurationMapper;
+
+    @InjectMocks
+    private DataPointConfigurationMapperImpl mapper;
+
+    @Test
+    void shouldMap() {
+        final DataPointDefinition dataPointDefinition = Instancio.create(DataPointDefinition.class);
+
+        final ExtractionConfiguration extractionConfiguration = mock();
+        when(extractionConfigurationMapper.map(dataPointDefinition.getExtractionDefinition())).thenReturn(extractionConfiguration);
+
+        final DataPointConfiguration dataPointConfiguration = mapper.map(dataPointDefinition);
+
+        assertEquals(dataPointDefinition.getField(),  dataPointConfiguration.getField());
+        assertEquals(dataPointDefinition.getSelector(), dataPointConfiguration.getSelector());
+        assertEquals(dataPointDefinition.getAttribute(), dataPointConfiguration.getAttribute());
+        assertEquals(dataPointDefinition.getType(), dataPointConfiguration.getType());
+        assertSame(extractionConfiguration, dataPointConfiguration.getExtractionConfiguration());
+    }
+}
