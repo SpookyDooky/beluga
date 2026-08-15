68322004a1615d511619b6634c1f10f4225efc8b
SpookyDooky <email@example.com>
2026-08-15 13:00:05 +0200

    Fix tons of test by adding json subtype information to dto's sadly this was required

--- src/beluga-scraper/src/main/java/com/beluga/api/job/dto/read/extraction_configuration/ReadExtractionConfigurationDto.java
+++ src/beluga-scraper/src/main/java/com/beluga/api/job/dto/read/extraction_configuration/ReadExtractionConfigurationDto.java
@@ -1,7 +1,26 @@
 package com.beluga.api.job.dto.read.extraction_configuration;
 
+import com.beluga.api.job.dto.read.extraction_configuration.attribute.ReadAttributeExtractionConfigurationDto;
+import com.beluga.api.job.dto.read.extraction_configuration.description_list.ReadDescriptionListExtractionConfigurationDto;
+import com.beluga.api.job.dto.read.extraction_configuration.html.ReadHtmlExtractionConfigurationDto;
+import com.beluga.api.job.dto.read.extraction_configuration.image.ReadImageExtractionConfigurationDto;
+import com.beluga.api.job.dto.read.extraction_configuration.text.ReadTextExtractionConfigurationDto;
 import com.beluga.model.job_definition.configuration.scraping_configuration.ExtractionType;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "type"
+)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = ReadAttributeExtractionConfigurationDto.class, name = "ATTRIBUTE"),
+        @JsonSubTypes.Type(value = ReadDescriptionListExtractionConfigurationDto.class, name = "DESCRIPTION_LIST"),
+        @JsonSubTypes.Type(value = ReadHtmlExtractionConfigurationDto.class, name = "HTML"),
+        @JsonSubTypes.Type(value = ReadImageExtractionConfigurationDto.class, name = "IMAGE"),
+        @JsonSubTypes.Type(value = ReadTextExtractionConfigurationDto.class, name = "TEXT"),
+})
 public abstract class ReadExtractionConfigurationDto {
 
     private Long id;
