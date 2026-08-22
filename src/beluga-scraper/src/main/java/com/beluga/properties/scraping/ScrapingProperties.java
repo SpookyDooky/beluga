4a93735d6fd68751df94bc84db35b494cfb431bb
SpookyDooky <email@example.com>
2026-08-22 11:15:55 +0200

    Fix all compilation errors

--- src/beluga-scraper/src/main/java/com/beluga/properties/scraping/ScrapingProperties.java
+++ src/beluga-scraper/src/main/java/com/beluga/properties/scraping/ScrapingProperties.java
@@ -1,17 +1,11 @@
 package com.beluga.properties.scraping;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-
-import java.util.List;
 
 public class ScrapingProperties {
 	
 	@NotBlank
 	private String itemSelector;
-	@NotEmpty
-	private List<@Valid DataPointProperties> dataPoints;
 	
 	public String getItemSelector() {
 		return itemSelector;
@@ -19,13 +13,5 @@ public class ScrapingProperties {
 	
 	public void setItemSelector(final String itemSelector) {
 		this.itemSelector = itemSelector;
-	}
-	
-	public List<DataPointProperties> getDataPoints() {
-		return dataPoints;
-	}
-	
-	public void setDataPoints(final List<DataPointProperties> dataPoints) {
-		this.dataPoints = dataPoints;
 	}
 }
--- src/beluga-scraper/src/test/java/com/beluga/mapper/job/scraping_configuration/ScrapingDefinitionMapperTest.java
+++ src/beluga-scraper/src/test/java/com/beluga/mapper/job/scraping_configuration/ScrapingDefinitionMapperTest.java
@@ -3,10 +3,10 @@ package com.beluga.mapper.job.scraping_configuration;
 import com.beluga.api.job.dto.read.ReadScrapingConfigurationDto;
 import com.beluga.api.job.dto.read.extraction_configuration.ReadExtractionConfigurationDto;
 import com.beluga.api.job.dto.write.WriteScrapingConfigurationDto;
+import com.beluga.api.job.dto.write.extraction_configuration.WriteExtractionConfigurationDto;
 import com.beluga.mapper.job.scraping_configuration.extraction_definition.ExtractionDefinitionMapper;
 import com.beluga.model.job_definition.configuration.scraping_configuration.ScrapingDefinition;
 import com.beluga.model.job_definition.configuration.scraping_configuration.extraction_definition.ExtractionDefinition;
-import com.beluga.properties.scraping.ScrapingProperties;
 import org.assertj.core.api.Assertions;
 import org.instancio.Instancio;
 import org.junit.jupiter.api.Test;
@@ -27,35 +27,21 @@ import static org.mockito.Mockito.when;
 class ScrapingDefinitionMapperTest {
 
 	@Mock
-	private DataPointDefinitionMapper dataPointDefinitionMapper;
-	@Mock
 	private ExtractionDefinitionMapper extractionDefinitionMapper;
 
 	@InjectMocks
 	private ScrapingDefinitionMapperImpl mapper;
 	
 	@Test
-	void shouldMap() {
-		final ScrapingProperties dataScrapingProperties = Instancio.of(ScrapingProperties.class)
-				.set(field(ScrapingProperties::getDataPoints), List.of(mock(DataPointProperties.class)))
-				.create();
-		
-		final DataPointDefinition dataPointDefinition = mock();
-		when(dataPointDefinitionMapper.map(dataScrapingProperties.getDataPoints().getFirst())).thenReturn(dataPointDefinition);
-		
-		final ScrapingDefinition scrapingDefinition = mapper.map(dataScrapingProperties);
-		
-		assertEquals(dataScrapingProperties.getItemSelector(), scrapingDefinition.getItemSelector());
-		assertEquals(1, scrapingDefinition.getExtractionDefinitions().size());
-		assertSame(dataPointDefinition, scrapingDefinition.getExtractionDefinitions().getFirst());
-
+	void shouldMapFromProperties() {
+		// ExtractionProperties
 		Assertions.fail();
 	}
 	
 	@Test
 	void shouldMapFromWriteScrapingConfigurationDto() {
 		final WriteScrapingConfigurationDto dto = Instancio.of(WriteScrapingConfigurationDto.class)
-				.set(field(WriteScrapingConfigurationDto::getDataPoints), List.of(mock(WriteDataPointConfigurationDto.class)))
+				.set(field(WriteScrapingConfigurationDto::getDataPoints), List.of(mock(WriteExtractionConfigurationDto.class)))
 				.create();
 		
 		final ExtractionDefinition extractionDefinition = mock();
@@ -71,7 +57,7 @@ class ScrapingDefinitionMapperTest {
 	@Test
 	void shouldMapToDto() {
 		final ScrapingDefinition entity = Instancio.of(ScrapingDefinition.class)
-				.set(field(ScrapingDefinition::getExtractionDefinitions), List.of(mock(DataPointDefinition.class)))
+				.set(field(ScrapingDefinition::getExtractionDefinitions), List.of(mock(ExtractionDefinition.class)))
 				.create();
 		
 		final ReadExtractionConfigurationDto extractionConfigurationDto = mock();
@@ -88,7 +74,7 @@ class ScrapingDefinitionMapperTest {
 	@Test
 	void shouldUpdate() {
 		final WriteScrapingConfigurationDto dto = Instancio.of(WriteScrapingConfigurationDto.class)
-				.set(field(WriteScrapingConfigurationDto::getDataPoints), List.of(mock(WriteDataPointConfigurationDto.class)))
+				.set(field(WriteScrapingConfigurationDto::getDataPoints), List.of(mock(WriteExtractionConfigurationDto.class)))
 				.create();
 		
 		final ExtractionDefinition extractionDefinition = mock();
