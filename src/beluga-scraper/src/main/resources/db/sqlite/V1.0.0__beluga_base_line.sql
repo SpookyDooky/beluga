60bedf351bc0a1e55ac35f5c8b39e139a3725350
SpookyDooky <email@example.com>
2026-09-09 08:33:21 +0200

    Clean up migration baseline sql scripts

--- src/beluga-scraper/src/main/resources/db/sqlite/V1.0.0__beluga_base_line.sql
+++ src/beluga-scraper/src/main/resources/db/sqlite/V1.0.0__beluga_base_line.sql
@@ -73,23 +73,15 @@ CREATE TABLE execution_definition
     tasks_per_second float   NOT NULL
 );
 
-CREATE TABLE url_configuration
-(
-    id       INTEGER PRIMARY KEY,
-    url_file varchar NULL
-);
-
 CREATE TABLE job_definition
 (
     id                      INTEGER PRIMARY KEY,
     "name"                  varchar NOT NULL,
     scraping_definition_id  integer NOT NULL,
     execution_definition_id integer NOT NULL,
-    url_configuration_id    integer,
     CONSTRAINT job_definition_name_key UNIQUE (name),
     FOREIGN KEY (execution_definition_id) REFERENCES execution_definition (id),
-    FOREIGN KEY (scraping_definition_id) REFERENCES scraping_definition (id),
-    FOREIGN KEY (url_configuration_id) REFERENCES url_configuration (id)
+    FOREIGN KEY (scraping_definition_id) REFERENCES scraping_definition (id)
 );
 
 CREATE TABLE job_execution
@@ -131,3 +123,7 @@ CREATE TABLE result_file
     key               varchar NULL,
     FOREIGN KEY (task_execution_id) REFERENCES task_execution (id)
 );
+
+create index if not exists job_execution_id_index on task_execution(job_execution_id);
+create index if not exists task_execution_id_index on result_file(task_execution_id);
+create index if not exists key_index on result_file(key);
